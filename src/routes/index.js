9a92df731f843ef8f5280f5ffa6b0b454c8d9bd2
ronaldalvareze <127575929+ronaldalvareze@users.noreply.github.com>
2023-08-24 18:48:24 -0500

    modificacion de pagina index.ejs

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,8 +5,10 @@ const router = Router()
 
 router.get('/', (req,res) => res.render('index',{ title: 'Bienvenido Al Drugs Preventions'}))
 router.get('/about', (req,res) => res.render('about', { title: 'Information'}))
-router.get('/contact', (req,res) => res.render('contact', { title: 'Contact'}))
 router.get('/service', (req,res) => res.render('service', { title: 'Service'}))
+router.get('/contact', (req,res) => res.render('contact', { title: 'Contact'}))
+
+
 
 
 export default router
