39f8a3857794ab908085f22c8cb1dce2b0256786
Lbahena <crazylucas_99@hotmail.com>
2023-08-23 22:12:32 -0500

    Actualizacion final del api

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,8 +3,10 @@ import{ Router } from "express"
 const router = Router()
 
 
-router.get('/', (req,res) => res.render('index',{ title: 'Bienvenido Al Index'}))
-router.get('/about', (req,res) => res.render('about', { title: 'Pagina About Me'}))
-router.get('/contact', (req,res) => res.render('contact', { title: 'Pagina Contact Me'}))
+router.get('/', (req,res) => res.render('index',{ title: 'Bienvenido Al Drugs Preventions'}))
+router.get('/about', (req,res) => res.render('about', { title: 'Information'}))
+router.get('/contact', (req,res) => res.render('contact', { title: 'Contact'}))
+router.get('/service', (req,res) => res.render('service', { title: 'Service'}))
+
 
 export default router
